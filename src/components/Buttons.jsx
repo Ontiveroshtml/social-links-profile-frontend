c0cbfb932857b838862c5501e0e36380d32ef965
Ontiveroshtml <velko.lords@gmail.com>
2025-11-09 19:08:20 -0600

    change steam button to Portfolio

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -2,7 +2,9 @@ export function Buttons({ item }) {
   return (
     <>
       <button className="bg-Grey-700 rounded-lg md:w-md p-4 w-sm text-white text-xl font-medium cursor-pointer hover:bg-Green hover:text-Grey-900">
-        <a href={item?.url} target="_blank">{item?.title}</a>
+        <a href={item?.url} target="_blank" rel="noreferrer">
+          {item?.title}
+        </a>
       </button>
     </>
   );
