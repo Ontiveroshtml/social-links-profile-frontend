3991f1208635957e87992f796369783672965713
Ontiveroshtml <velko.lords@gmail.com>
2025-11-09 19:04:03 -0600

    add padding to card container

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ import photoUser from "../assets/images/118926595.jpeg";
 export function Card() {
   return (
     <>
-      <div className="flex flex-col max-w-xl items-center gap-6 bg-Grey-800 mx-auto">
+      <div className="flex flex-col max-w-xl items-center gap-6 bg-Grey-800 mx-auto p-16">
         <img src={photoUser} alt="" className="max-w-40 rounded-full" />
         <span className="text-Green font-bold text-lg">Durango, México</span>
         <p className="text-white opacity-80 font-medium">
